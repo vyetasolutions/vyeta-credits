256f51135d8f9c74eb927ac63a89fb9ae681c105
vyetasolutions <vyeta.tech@gmail.com>
2026-07-22 19:07:42 +0200

    Add minimal service worker for installability

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+// Minimal service worker to enable installability
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let network handle fetches
+});
